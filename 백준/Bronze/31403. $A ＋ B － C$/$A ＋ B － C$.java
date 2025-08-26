b206b0ed33c96ec7e27c9c3ef08ab3cd13c95bb4
k0081915 <127379428+k0081915@users.noreply.github.com>
2025-08-26 16:09:11 +0900

    [Bronze IV] Title: $A + B - C$, Time: 128 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        String AB = A + String.valueOf(B);
+
+
+        System.out.println(A+ B - C);
+        System.out.println(Integer.parseInt(AB) - C);
+    }
+}
