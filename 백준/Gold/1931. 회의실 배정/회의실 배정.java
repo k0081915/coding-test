1e48748f7bb20c8f7ce8d7efd63d8c4eb4756f97
k0081915 <127379428+k0081915@users.noreply.github.com>
2025-09-05 11:18:05 +0900

    [Gold V] Title: 회의실 배정, Time: 492 ms, Memory: 43804 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -12,9 +12,11 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
+        // 회의 수 입력
         N = Integer.parseInt(br.readLine());
         work = new int[N][2];
 
+        // 회의 시간 입력
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 2; j++) {
@@ -22,11 +24,16 @@ public class Main {
             }
         }
 
+        // 끝나는 시간을 기준으로 오름차순 정렬
+        // 끝나는 시간이 같으면 시작 시간을 기준으로 정렬
         Arrays.sort(work, (o1, o2) -> o1[1] == o2[1] ? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1]));
 
         int cnt = 0;
         int end = 0;
         for (int i = 0; i < N; i++) {
+            // 끝나는 시간이 다음 회의 시작 시간보다 작거나 같으면
+            // 횟수를 늘리고
+            // 끝나는 시간 갱신
             if (end <= work[i][0]) {
                 cnt++;
                 end = work[i][1];
