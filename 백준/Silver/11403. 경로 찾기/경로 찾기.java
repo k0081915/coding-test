7e0f6013601f4b2cca67238fc2e9f5e8ca98b387
k0081915 <127379428+k0081915@users.noreply.github.com>
2025-09-12 16:26:33 +0900

    [Silver I] Title: 경로 찾기, Time: 188 ms, Memory: 17020 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -4,59 +4,37 @@ import java.util.*;
 
 public class Main {
 
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-	static int[][] graph2;
-	static int[][] result;
+	static int[][] arr;
+
+	static int N;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(br.readLine());
-		graph2 = new int[N][N];
-		result = new int[N][N];
-
-		for (int i = 0; i < N; i++) {
-			graph.add(new ArrayList<>());
-		}
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				graph2[i][j] = Integer.parseInt(st.nextToken());
-//				if (Integer.parseInt(st.nextToken()) == 1) {
-//					graph.get(i).add(j);
-//					graph.get(j).add(i);
-//					graph2[i][j] = graph2[j][i] = 1;
-//				}
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-
-//		for (int i = 0; i < graph.size(); i++) {
-//			System.out.println("index = " + i + " values = " + graph.get(i));
-//			bfs(i);
-//		}
-
-//		for (int[] ints : result) {
-//			for (int num : ints) {
-//				sb.append(num + " ");
-//			}
-//			sb.append('\n');
-//		}
-
+		// 플로이드-워셜 알고리즘
 		for (int k = 0; k < N; k++) {
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if (graph2[i][k] == 1 && graph2[k][j] == 1) {
-						graph2[i][j] = 1;
+					if (arr[i][k] == 1 && arr[k][j] == 1) {
+						arr[i][j] = 1;
 					}
 				}
 			}
 		}
 
-		for (int[] ints : graph2) {
+		for (int[] ints : arr) {
 			for (int num : ints) {
 				sb.append(num + " ");
 			}
@@ -67,16 +45,4 @@ public class Main {
 		br.close();
 	}
 
-	static void bfs(int node) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(node);
-
-		while (!queue.isEmpty()) {
-			int idx = queue.remove();
-			ArrayList<Integer> list = graph.get(idx);
-			for (Integer i : list) {
-				result[node][i] = 1;
-			}
-		}
-	}
 }
