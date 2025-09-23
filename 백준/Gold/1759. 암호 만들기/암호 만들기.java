7999d85868b2aa9ac2e08efa8f194d9d95f84dee
k0081915 <127379428+k0081915@users.noreply.github.com>
2025-09-23 10:36:49 +0900

    [Gold V] Title: 암호 만들기, Time: 116 ms, Memory: 14648 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+	static int L, C;
+	static String[] input;
+	static String[] arr;
+
+	static boolean[] visited;
+
+	static StringBuilder sb = new StringBuilder();
+
+	static Set<String> aeiou = new HashSet<>(Arrays.asList("a", "e", "i", "o", "u"));
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+//		StringBuilder sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine());
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		arr = new String[C];
+		input = new String[C];
+		visited = new boolean[C];
+
+		input = br.readLine().split(" ");
+		Arrays.sort(input);
+
+
+		for (int i = 0; i < C; i++) {
+			for (int j = 0; j < C; j++) {
+
+			}
+		}
+		permu(0, 0);
+		System.out.println(sb);
+
+		br.close();
+	}
+
+	static void permu(int idx, int start) {
+		if (idx == L) {
+			// 최소 1개 모음, 최소 2개 자음 체크
+			if (check()) {
+				for (int i = 0; i < L; i++) {
+					sb.append(arr[i]);
+				}
+				sb.append("\n");
+			}
+
+			return;
+		}
+
+		for (int i = start; i < C; i++) {
+			if(visited[i]) continue;
+			arr[idx] = input[i];
+			visited[i] = true;
+			permu(idx + 1, i);
+			visited[i] = false;
+		}
+	}
+
+	static boolean check() {
+		int cnt1 = 0, cnt2 = 0;
+		for (int i = 0; i < L; i++) {
+			if (aeiou.contains(arr[i])) {
+				cnt1++;
+			} else {
+				cnt2++;
+			}
+		}
+		return cnt1 >= 1 && cnt2 >= 2;
+	}
+
+}
