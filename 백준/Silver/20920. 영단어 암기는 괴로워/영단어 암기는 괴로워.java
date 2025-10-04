7b73e24b650c1924e3de1473364644366644ca8b
Donghyeok Kim <127379428+k0081915@users.noreply.github.com>
2025-10-04 16:07:22 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 648 ms, Memory: 37280 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -11,7 +11,6 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 
 		Map<String, Integer> map = new HashMap<>();
-		List<String> list;
 
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
@@ -20,23 +19,31 @@ public class Main {
 			String str = br.readLine();
 			if(str.length() < M) continue;
 
+			// map에 추가
+			// map에 str이 이미 존재하면 value + 1
 			map.put(str, map.getOrDefault(str, 1) + 1);
-			
 		}
 
-		list = new ArrayList<>(map.keySet());
+		// 리스트에 map의 키들을 추가해줌
+		List<String> list = new ArrayList<>(map.keySet());
 
 		list.sort((o1, o2) -> {
+			// 단어 나온 횟수가 같으면
 			if (Objects.equals(map.get(o1), map.get(o2))) {
+				// 단어 길이가 같으면
 				if (o1.length() == o2.length()) {
+					// 사전순으로 앞인 단어를 앞으로
 					return o1.compareTo(o2);
-				} else {
+				} else { // 단어 길이가 다르면
+					// 단어 길이가 긴 것을 앞으로
 					return Integer.compare(o2.length(), o1.length());
 				}
 			}
+			// 단어 나온 횟수가 많은 것을 앞으로
 			return Integer.compare(map.get(o2), map.get(o1));
 		});
 
+		// 리스트 출력
 		for (String s : list) {
 			sb.append(s).append("\n");
 		}
