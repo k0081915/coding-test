46893756ca8870d30d1007e5158a4c43b85cb5c9
Donghyeok Kim <127379428+k0081915@users.noreply.github.com>
2026-03-22 18:13:18 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 540 ms, Memory: 54924 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -34,47 +34,45 @@ class Main {
                 }
                 heights[idx++] = Long.parseLong(st.nextToken());
             }
-            
-            // 스택에는 인덱스를 저장
-            Stack<Integer> stack = new Stack<>();
-            long maxArea = 0;
-            
-            // 왼쪽부터 막대를 하나씩 확인
-            for(int i = 0; i < n; i++) {
-                
-                // 현재 막대가 스택 top 막대보다 낮아지는 순간
-                // top 막대를 높이로 하는 최대 직사각형 넓이를 계산할 수 있다
-                while(!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
-                    long height = heights[stack.pop()];
-                    
-                    // 스택이 비어있으면
-                    // 현재 막대 i 이전까지 전부 확장 가능 -> 너비 = i
-                    // 비어있지 않으면
-                    // stack.peek() 다음부터 i-1까지 확장 가능
-                    int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-                    
-                    maxArea = Math.max(maxArea, height * width);
-                }
-                
-                // 현재 막대 인덱스를 스택에 넣는다
-                stack.push(i);
-            }
-            
-            // 끝까지 다 봤는데도 스택에 남아있는 막대들 처리
-            // 이 막대들은 오른쪽 끝(n-1)까지 확장 가능하다
-            while(!stack.isEmpty()) {
-                long height = heights[stack.pop()];
-                
-                // 스택이 비었으면 처음부터 끝까지 확장 가능 -> 너비 = n
-                // 비어있지 않으면 stack.peek()+1 부터 n-1까지 확장 가능
-                int width = stack.isEmpty() ? n : n - stack.peek() - 1;
-                
-                maxArea = Math.max(maxArea, height * width);
-            }
-            
-            sb.append(maxArea).append('\n');
+
+            sb.append(getMaxArea(heights)).append('\n');
         }
         
         System.out.println(sb);
+    }
+    
+    static long getMaxArea(long[] heights) {
+        int n = heights.length;
+        // 스택에는 인덱스를 저장
+        Deque<Integer> stack = new ArrayDeque<>();
+        long maxArea = 0L;
+
+        // 왼쪽부터 막대를 하나씩 확인
+        for(int i = 0; i <= n; i++) {
+            // i == n 일때 높이 0인 막대를 하나 본다고 가정해서
+            // 스택에 남아있는 모든 막대를 한 번에 정리한다
+            long currentHeight = (i == n) ? 0 : heights[i];
+
+            // 현재 막대가 스택 top 막대보다 낮아지는 순간
+            // top 막대를 높이로 하는 최대 직사각형 넓이를 계산할 수 있다
+            while(!stack.isEmpty() && heights[stack.peek()] > currentHeight) {
+                long height = heights[stack.pop()];
+
+                // 스택이 비어있으면
+                // 현재 막대 i 이전까지 전부 확장 가능 -> 너비 = i
+                // 비어있지 않으면
+                // stack.peek() 다음부터 i-1까지 확장 가능
+                int leftBoundary = stack.isEmpty() ? -1 : stack.peek();
+                
+                int width = i - leftBoundary - 1;
+
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            // 현재 막대 인덱스를 스택에 넣는다
+            stack.push(i);
+        }
+        
+        return maxArea;
     }
 }
